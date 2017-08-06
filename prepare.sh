cfcb841673c1361f11839b28f670152faad7acf9
Park Sung Min <vamf12@gmail.com>
2017-08-06 17:54:07 +0900

    chamge system type name

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "rootProject.name='kelibs-result'\ninclude 'jdk', 'js'" > settings.gradle
+echo "rootProject.name='kelibs-result'\ninclude 'java', 'js'" > settings.gradle
 touch build.gradle
 jdk/gradlew wrapper
