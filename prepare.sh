28ffaafc90683611f9ba97221a6191264db614a9
Park Sung Min <vamf12@gmail.com>
2017-10-24 15:50:28 +0900

    fix jitpack prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@
 
 echo "rootProject.name='kelibs-result'\ninclude 'kelibs-result-java', 'kelibs-result-js'" > settings.gradle
 touch build.gradle
-java/gradlew wrapper
+kelibs-result-java/gradlew wrapper
